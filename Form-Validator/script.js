528cbbcb4a02c7c41a0e68d664384f26be1380f8
Anthony Munnings <anthonymunnings@gmail.com>
2022-05-16 13:27:31 -0400

    passwords-fixed

--- Form-Validator/script.js
+++ Form-Validator/script.js
@@ -21,9 +21,13 @@ function showSuccess(input){
 }
 
 // check valid email
-function isValidEmail(email){
+function CheckEmail(input){
    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return re.test(String(email).toLowerCase());
+   if (re.test(input.value.trim())) {
+       showSuccess(input);
+}else {
+    showError(input, 'Email is invalid');
+   }
     
 }
 
@@ -39,6 +43,25 @@ inputArr.forEach(function(input) {
 });
 }
 
+
+// Check Passwords Match
+function checkPasswordsMatch(input1, input2) {
+ if(input1.value !== input2.value){
+     showError(input2, 'Passwords dont match');
+ } 
+}
+
+// CHECK INPUT LENGTH
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be atleast ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
 // GET FIELD NAME
 // This function capitalizes the first letter of each id that displays in the error message
  function getFieldName(input) {
@@ -51,5 +74,9 @@ form.addEventListener('submit', function(e) {
  e.preventDefault();
 
 checkRequired([username, email, password, password2]);
+checkLength(username, 3, 15);
+checkLength(password, 6, 25);
+CheckEmail(email);
+checkPasswordsMatch(password, password2);
 
 });
